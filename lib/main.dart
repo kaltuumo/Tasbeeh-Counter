c24fb97b18670c921ba650255ca4b74517476f7e
Kaltuumo <kaltuumo905@gmail.com>
2024-12-09 22:53:31 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tasbeeh_counter/homescreen.dart';
+// import 'package:tasbih/homescreen.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
